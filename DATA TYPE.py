95acb059b16a9ade5636a0d4483d39647be8bff1
Emmanuel Obiajulu Okoye <>
2021-04-06 19:56:09 +0000

    Initial commit

---
+++ DATA TYPE.py
@@ -0,0 +1,8 @@
+number = input ("Type  a two digit Number\n")
+# this section of code try to get the first and second subscript
+first_digit = number[0]
+second_digit = number[1]
+# in this section we converted the string to integer
+#and added the both of them which is the first and second input
+
+print (int(first_digit) + int(second_digit))
